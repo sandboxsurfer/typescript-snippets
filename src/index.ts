c82ecfb24ab8ded46a911464ceb124656a178281
sandboxsurfer <goalsofar@gmail.com>
2022-11-04 15:36:57 +0000

    test: add oneliners

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  Hello there
+  Hello there again
 </div>`;
 
 console.log("foo");
+console.log(foo('hello world'))
